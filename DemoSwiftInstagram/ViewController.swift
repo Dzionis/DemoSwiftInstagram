5c403d238c3c6adc9b89d580cbdd2c9bcd25d5ce
Dzionis Brek <dzionis.brek@effective-soft.com>
2017-10-13 13:18:00 +0300

    [+] Add ios project

---
+++ DemoSwiftInstagram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoSwiftInstagram
+//
+//  Created by Dzionis Brek on 13.10.2017.
+//  Copyright © 2017 Dzionis Brek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
